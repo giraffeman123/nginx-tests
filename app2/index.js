46c3d15621d5b8d34327ca25697fca749ad64339
giraffeman123 <elliotmtz12@gmail.com>
2022-12-23 01:07:31 -0800

    first commit

---
+++ app2/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api', (req, res) => {
+    res.status(200).json({info:"hello from app2"});
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
